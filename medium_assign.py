e59f851b7e9009bc38972ff1e2c214f827de9c98
jordanlleslie <78113256+jordanlleslie@users.noreply.github.com>
2021-02-10 21:19:58 -0500

    defined functions and implemented function call

--- medium_assign.py
+++ medium_assign.py
@@ -15,28 +15,36 @@ DO NOT EDIT THE FUNCTION NAMES.
 
 def expression_1(x):
 
-    '''
-    
-        Write a code that returns value for the following expression: x^3 - (2x + x^2) - 100 
-    
-    '''
+     #Write a code that returns value for the following expression: x^3 - (2x + x^2) - 100 
+    result = x ** 3 - (2 * x + x ** 2) - 100
+    return result
 
-    pass
 
 def expression_2(x, y):
 
-    '''
-        Write code that returns only the integer value of the following expression: (x^4 / 2y) - (3xy) + (6y / 7x)
-    '''
-
-    pass
+        #Write code that returns only the integer value of the following expression: (x^4 / 2y) - (3xy) + (6y / 7x)
+    result = (x ** 4)/(2 * y) - 3 * x * y + (6 * y)/(7 * x)
+    return result
 
 
 def expression_3(x, y):
-    '''
-        Write code that returns the value of the following expression: (x^3 + y^2) / (x^2 - y^3)
-    '''
+
+       # Write code that returns the value of the following expression: (x^3 + y^2) / (x^2 - y^3)
+    result = (x ** 3 + y ** 2) / (x ** 2 - y ** 3)
+    return result
 
 if __name__ == "__main__":
     
-    pass
+    x = float(input('Enter a value: '))
+    result_1 = expression_1(x)
+    print(f'Result 1 is {result_1}')
+
+    x_1 = float(input('Enter x value: '))
+    y_1 = float(input('Enter y value: '))
+    result_2 = expression_2(x_1, y_1)
+    print(f'Result 2 is {result_2}')
+
+    x_2 = float(input('Enter x value: '))
+    y_2 = float(input('Enter y value: '))
+    result_3 = expression_3(x_2, y_2)
+    print(f'Result 3 is {result_3}')
